ca5e049b4dc2e4db9362f0024d55b18b804d3677
Giovane_Bez123 <61485021+GiovaneBez123@users.noreply.github.com>
2022-03-21 23:37:33 -0300

    Create PROBLEMA_08.py

---
+++ PROBLEMA_08.py
@@ -0,0 +1,11 @@
+# Desenvolva um algoritmo e um fluxograma para um programa que permita com que o usuário insira quatro números.
+# O programa deverá imprimir a média ponderada, sabendo-se que os pesos são respectivamente: 1, 2, 3 e 4.
+
+valor1 = float(input("digite um numero1:"))
+valor2 = float(input("digite um numero2:"))
+valor3 = float(input("digite um numero3:"))
+valor4 = float(input("digite um numero4:"))
+
+calculo = (valor1 + valor2 + valor3 + valor4)/10
+
+print("o valor da média é:",calculo)
